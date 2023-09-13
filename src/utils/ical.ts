e283af252a87238a2c73dd2870ef9ac86678c174
Cattiwi <38155922+Diegomcha@users.noreply.github.com>
2023-09-13 16:25:46 +0200

    Various changes
    - Moved to 'eiical'
    - Added alerts 15min before each class

--- src/utils/ical.ts
+++ src/utils/ical.ts
@@ -21,13 +21,17 @@ function parseEvent(raw: string[]): string {
 	return [
 		'BEGIN:VEVENT',
 		`UID:${uuid()}`,
-		'SEQUENCE:0',
 		`SUMMARY:${raw[0]}`,
 		`DESCRIPTION:${raw[5]}`,
 		`LOCATION:${raw[6]}`,
 		`DTSTAMP:${dayjs().format('YYYYMMDD[T]HHmmss')}`,
 		`DTSTART:${start}`,
 		`DTEND:${end}`,
+		'BEGIN:VALARM',
+		'TRIGGER:-PT15M',
+		'ACTION:DISPLAY',
+		`DESCRIPTION:${raw[0]}`,
+		'END:VALARM',
 		'END:VEVENT',
 	].join('\r\n');
 }
@@ -43,7 +47,9 @@ export function generateIcalString(data: Data, csvData: string): string {
 		'VERSION:2.0',
 		'PRODID:-//EII-CAL//eii-cal//EN',
 		`NAME:[${data.year}|${data.semester}] EII Schedule for ${data.user}`,
+		`X-WR-CALNAME:[${data.year}|${data.semester}] EII Schedule for ${data.user}`,
 		'TIMEZONE-ID:Europe/Madrid',
+		'X-WR-TIMEZONE:Europe/Madrid',
 		events,
 		'END:VCALENDAR',
 	].join('\r\n');
